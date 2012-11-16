720e96e9a1f4ee2a369100b39c3adf82df4e8c5c
Antti Virtanen <antti.virtanen@iki.fi>
2012-11-16 15:20:45 +0200

    new constructor, getters

--- src/scripts/app/gwparameter.js
+++ src/scripts/app/gwparameter.js
@@ -1,17 +1,19 @@
-// global warming params. 
-// aluksi vain yksi, refaktoroidaan kun saadaan joku runko kasaan
+// global warming params. or was it local warming. Heat is on anyway.
 define(function() {
         var name = "Traktori";
-        var  size = 4; // 1-5
+        var value = 4; // 1-5
         var coMultiplier = 1000; // 1000g * size
         var rangeMin = 1;
         var rangeMax = 5;
 	// rakentaja
-	function GlobalWarmingParameter(name) {
+	function GlobalWarmingParameter(name, rangeMin, rangeMax, value, coMultiplier) {
 	  this.name = name;
+      this.rangeMin = rangeMin;
+      this.rangeMax = rangeMax;
+      this.value = value;
+      this.coMultiplier = coMultiplier;
 	  return (this);
 	}
-	
 	
 	GlobalWarmingParameter.prototype.getName = function() {
             	return name;
@@ -19,18 +21,19 @@ define(function() {
 	GlobalWarmingParameter.prototype.getMin = function() {
 	    return rangeMin;
 	};
-	GlobalWarmingParameter.prototype.getMax = function() {
-	    return rangeMax;
-	};
+    GlobalWarmingParameter.prototype.getMax = function() {
+        return rangeMax;
+    };
+    GlobalWarmingParameter.prototype.getEmission = function() {
+        return value * coMultiplier;
+    };
+    GlobalWarmingParameter.prototype.getValue = function() {
+        return value;
+    };
+    GlobalWarmingParameter.prototype.getCoMultiplier = function() {
+        return coMultiplier;
+    };
+
 	return (GlobalWarmingParameter);
-
-	/*
-            getEmission : function() {
-            	return coMultiplier * size;
-            }
-	*/
-
-
-        }
-    
+    }    
 );
--- src/scripts/app/megamain.js
+++ src/scripts/app/megamain.js
@@ -3,7 +3,7 @@ define(['jquery', 'gwparameter'],
 	function($, GlobalWarmingParameter) {
 	    var megamain = {
 	    	init: function(){
-	    		 var p = new GlobalWarmingParameter("kökkötraktori");
+	    		 var p = new GlobalWarmingParameter("kökkötraktori", 1, 5, 4, 1000);
 			     $("#traktori").prop("min", p.getMin());
 			     $("#traktori").prop("max", p.getMax());
 
