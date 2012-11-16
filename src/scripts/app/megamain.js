01da6d1ce0d74c1d8f13aa6764e19dec37d8c889
Antti Virtanen <antti.virtanen@iki.fi>
2012-11-16 15:36:50 +0200

    taulukointi arvoille

--- src/scripts/app/megamain.js
+++ src/scripts/app/megamain.js
@@ -1,9 +1,16 @@
 
 define(['jquery', 'gwparameter'], 
 	function($, GlobalWarmingParameter) {
+		var parameters = new Array(		  
+			new GlobalWarmingParameter("kökkötraktori", 1, 5, 4, 1000),
+			new GlobalWarmingParameter("auto", 1, 5, 4, 100),
+			new GlobalWarmingParameter("sähkönkulutus", 1, 5000, 1500, 1)
+		);
+
 	    var megamain = {
-	    	init: function(){
-	    		 var p = new GlobalWarmingParameter("kökkötraktori", 1, 5, 4, 1000);
+	    	init: function() {
+	    		 var p = parameters[0];
+
 			     $("#traktori").prop("min", p.getMin());
 			     $("#traktori").prop("max", p.getMax());
 
