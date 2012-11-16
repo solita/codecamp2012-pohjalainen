1eaef5f41996b7800241d8a31b9966f1b5edbde8
Antti Virtanen <antti.virtanen@iki.fi>
2012-11-16 13:05:17 +0200

    structuree. first global warming param!!

---
+++ src/scripts/app/gwparameter.js
@@ -0,0 +1,7 @@
+// global warming params. 
+// aluksi vain yksi, refaktoroidaan kun saadaan joku runko kasaan
+define({
+	name: "Traktori",
+	size: 4, // 1-5
+	coMultiplier : 1000 // 1000g * size
+});
