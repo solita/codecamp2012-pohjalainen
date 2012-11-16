bf707fe4c2efc292530d623d2df7ee28090b0e6a
Antti Virtanen <antti.virtanen@iki.fi>
2012-11-16 14:08:32 +0200

    syntax error

--- src/scripts/app/gwparameter.js
+++ src/scripts/app/gwparameter.js
@@ -2,8 +2,8 @@
 // aluksi vain yksi, refaktoroidaan kun saadaan joku runko kasaan
 define(function() {
         var name = "Traktori";
-        var  size: 4; // 1-5
-        var coMultiplier : 1000; // 1000g * size
+        var  size = 4; // 1-5
+        var coMultiplier = 1000; // 1000g * size
         var rangeMin = 1;
         var rangeMax = 5;
         return {
