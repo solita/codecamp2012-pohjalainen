809a2cc8ac54476b3203c06e5329faf0ff6711d1
Antti Virtanen <antti.virtanen@iki.fi>
2012-11-16 13:19:55 +0200

    getterit

--- src/scripts/app/gwparameter.js
+++ src/scripts/app/gwparameter.js
@@ -1,7 +1,23 @@
 // global warming params. 
 // aluksi vain yksi, refaktoroidaan kun saadaan joku runko kasaan
-define({
-	name: "Traktori",
-	size: 4, // 1-5
-	coMultiplier : 1000 // 1000g * size
-});
+define(function() {
+        //return an object to define the gw param module
+        return {
+			name: "Traktori",
+			size: 4, // 1-5
+			coMultiplier : 1000, // 1000g * size
+            getName: function() {
+            	return this.name;
+            },
+            getMin: function() {
+            	return 1;
+            },
+            getMax: function() {
+            	return 5;
+            },
+            getEmission: function() {
+            	return coMultiplier * size;
+            }
+        }
+    }
+);
